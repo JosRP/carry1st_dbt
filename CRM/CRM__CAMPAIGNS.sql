44d8b9f7d0afb2ff1917656b0521e3e6c35ad327
Jose Gomes <jose@carry1st.com>
2026-02-13 12:07:57 +0000

    Add initial insert statement for CRM__CAMPAIGNS

---
+++ CRM/CRM__CAMPAIGNS.sql
@@ -0,0 +1,10 @@
+INSERT INTO carry1st_platform.refined.CRM__CAMPAIGNS 
+
+-- SELECT
+--     customer_id,
+--     last_recipient,
+--     push_token,
+--     variant_group,
+--     campaign_name,
+--     send_date
+-- FROM  carry1st_platform.refined.CRM__CAMPAIGNS
